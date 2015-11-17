510c1e499e47e9a51fe4e4e6d2764b6a3747ca70
jkiley129 <josephkiley@icloud.com>
2015-11-17 13:58:57 -0500

    Linked to Parse account

--- ProjectMailbox/ProjectMailbox/AppDelegate.m
+++ ProjectMailbox/ProjectMailbox/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import <Parse/Parse.h>
 
 @interface AppDelegate ()
 
@@ -14,9 +15,18 @@
 
 @implementation AppDelegate
 
-
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    // [Optional] Power your app with Local Datastore. For more info, go to
+    // https://parse.com/docs/ios_guide#localdatastore/iOS
+    [Parse enableLocalDatastore];
+    
+    // Initialize Parse.
+    [Parse setApplicationId:@"rQDnUltUD2fr1WN4XK1Dc8K3isG2ebpVeX1pLOF6"
+                  clientKey:@"ivu9n1BAvIFtH9Kcu9s9aBuY7Tq3Ue8gNnjOGHq6"];
+    
+    // [Optional] Track statistics around application opens.
+    [PFAnalytics trackAppOpenedWithLaunchOptions:launchOptions];
+    
     return YES;
 }
 
